0ec6525ba327b18320f39ab7196cd1d01f16e516
Abhishek Pingale <abhishek_pingale@persistent.com>
2020-04-03 15:47:05 +0530

    added set_tag function

--- examples/sklearn_elasticnet_wine/train.py
+++ examples/sklearn_elasticnet_wine/train.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
-
+        mlflow.set_tag(run_name,"mlflow-model-test")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
